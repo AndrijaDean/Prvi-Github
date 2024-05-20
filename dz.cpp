03c0ac051a0d5e794887ffcb194fe06254afee40
AndrijaDean <andrija.dean@gmail.com>
2024-05-20 10:56:47 +0200

    radi

--- dz.cpp
+++ dz.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 struct Ucenik
 {
-    char imePrezime[30];
+    char imePrezime[50];
     float prosjek;
 };
 bool cmpp(Ucenik &a, Ucenik &b)
@@ -17,7 +17,7 @@ int main()
 {
     struct Ucenik ucenici[100];
     int brUcenika = 0;
-    fstream datoteka("C:\\Users\\Ga-gama\\Documents\\GitHub\\Prvi-Github\\data.bin", ios::binary | ios::in);
+    fstream datoteka("C:/Users/Ga-gama/Documents/GitHub/Prvi-Github/data.bin", ios::binary | ios::in);
     while (datoteka.read((char *)&ucenici[brUcenika], sizeof(Ucenik)))
     {
         cout << ucenici[brUcenika].imePrezime << " " << ucenici[brUcenika].prosjek << endl;
@@ -29,12 +29,13 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        cin.getline(ucenici[brUcenika + i].imePrezime, 30);
+        cin.ignore();
+        cin.getline(ucenici[brUcenika + i].imePrezime, 50);
         cin >> ucenici[brUcenika + i].prosjek;
     }
     sort(ucenici, ucenici + brUcenika + n, cmpp);
-    fstream datoteka1("C:\\Users\\Ga-gama\\Documents\\GitHub\\Prvi-Github\\data.bin", ios::binary | ios::out | ios::trunc);
-    datoteka1.write((char *)&ucenici, sizeof(Ucenik) * (brUcenika + n));
+    fstream datoteka1("C:/Users/Ga-gama/Documents/GitHub/Prvi-Github/data.bin", ios::binary | ios::out | ios::trunc);
+    datoteka1.write((char *)ucenici, sizeof(Ucenik) * (brUcenika + n));
     datoteka1.close();
     return 0;
 }
